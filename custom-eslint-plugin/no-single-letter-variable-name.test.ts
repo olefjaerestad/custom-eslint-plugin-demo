8009709f65e1d9489db8d3936c93974a8cb419e8
Ole Kristian Fjærestad <fjaerestad.olekristian@gmail.com>
2023-06-16 14:26:04 +0200

    Fix incorrect import path.

--- custom-eslint-plugin/no-single-letter-variable-name.test.ts
+++ custom-eslint-plugin/no-single-letter-variable-name.test.ts
@@ -1,5 +1,5 @@
 import { RuleTester } from 'eslint';
-import rule from './no-update-penalty-directly';
+import rule from './no-single-letter-variable-name';
 
 const ruleTester = new RuleTester({
   parser: require.resolve('@typescript-eslint/parser'),
